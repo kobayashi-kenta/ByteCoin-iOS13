c17e252699c1fd27c02c57786a9bf05de3de2760
kobayashi-kenta <12321klj@gmail.com>
2020-12-30 17:03:13 +0900

    firstcommit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Kenta on 2020/12/30.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
